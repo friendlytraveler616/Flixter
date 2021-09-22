6130b279c056d972b14244f758bfa9dba8f62a64
friendlytraveler616 <53345715+friendlytraveler616@users.noreply.github.com>
2021-09-21 22:41:43 -0400

    change

---
+++ MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Joonho Jun on 9/21/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
